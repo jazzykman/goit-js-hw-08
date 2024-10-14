4cc42d8be956f7305c70dfcf3abc625e8d5d10aa
artem <jazzykman06@gmail.com>
2024-10-14 15:13:29 +0300

    summary

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const categoriesList = document.querySelectorAll("#categories .item");
-console.log(`Number of categories: ${categoriesList.length}`);
-
-categoriesList.forEach((category) => {
-  const categoryTitle = category.querySelector("h2").textContent;
-  const categoryItems = category.querySelectorAll("li").length;
-  console.log(`Category: ${categoryTitle}\nElements: ${categoryItems}`);
-});
--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const inputEl = document.querySelector("#name-input");
-const outputEl = document.querySelector("#name-output");
-
-inputEl.addEventListener("input", (event) => {
-  const inputValue = event.currentTarget.value.trim();
-  outputEl.textContent = inputValue ? inputValue : "Anonymous";
-});
