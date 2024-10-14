dd715fd17c1507692acad064c03bb792cccca93e
artem sychov <78472265+jazzykman@users.noreply.github.com>
2024-10-14 14:59:48 +0300

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+const categoriesList = document.querySelectorAll("#categories .item");
+console.log(`Number of categories: ${categoriesList.length}`);
+
+categoriesList.forEach((category) => {
+  const categoryTitle = category.querySelector("h2").textContent;
+  const categoryItems = category.querySelectorAll("li").length;
+  console.log(`Category: ${categoryTitle}\nElements: ${categoryItems}`);
+});
